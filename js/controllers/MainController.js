242636ef0e4575710245354d5d12ff811b560606
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-08-27 23:19:10 +0530

    Add prduct name and price

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Hello AngularJS!'; 
   $scope.promo = "Let's explore";
+  $scope.product = {
+	  name: 'The Book of Trees',
+	  price: 19
+  };
 }]);
