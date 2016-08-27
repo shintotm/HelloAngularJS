ec665627b08852e1943dcf3ca841f487e34b7960
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-08-28 00:16:55 +0530

    Add dislikes button

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,7 +7,8 @@ app.controller('MainController', ['$scope', function($scope) {
 	price: 19, 
 	pubdate: new Date('2014', '03', '08'), 
 	cover: 'img/the-book-of-trees.jpg' ,
-	likes: 0
+	likes: 0,
+    dislikes: 0
 	
   }, 
   { 
@@ -15,24 +16,30 @@ app.controller('MainController', ['$scope', function($scope) {
 	price: 8, 
 	pubdate: new Date('2013', '08', '01'), 
 	cover: 'img/program-or-be-programmed.jpg' ,
-	likes: 0
+	likes: 0,
+    dislikes: 0
   }, 
   { 
 	name: 'As a Man Thinketh', 
 	price: 5.4, 
 	pubdate: new Date('2013', '08', '01'), 
 	cover: 'http://awesomeaj.com/wp-content/uploads/2014/05/as-a-man-thinketh-cover.png' ,
-	likes: 0
+	likes: 0,
+    dislikes: 0
   }, 
   { 
 	name: 'Fifty Shades of Grey', 
 	price: 6.5, 
 	pubdate: new Date('2011', '08', '01'), 
 	cover: 'https://upload.wikimedia.org/wikipedia/en/5/5e/50ShadesofGreyCoverArt.jpg' ,
-	likes: 0
+	likes: 0,
+    dislikes: 0
   }  
 ];
 	$scope.plusOne = function(index) {
     $scope.products[index].likes +=1;
+  };
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes  +=1;
   };
 }]);
