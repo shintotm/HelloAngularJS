026635dbe3ce19372d838b41ea8f405125a7cac8
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-08-27 23:54:54 +0530

    Add two more books

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,6 +13,18 @@ app.controller('MainController', ['$scope', function($scope) {
 	price: 8, 
 	pubdate: new Date('2013', '08', '01'), 
 	cover: 'img/program-or-be-programmed.jpg' 
-  } 
+  }, 
+  { 
+	name: 'As a Man Thinketh', 
+	price: 5.4, 
+	pubdate: new Date('2013', '08', '01'), 
+	cover: 'http://awesomeaj.com/wp-content/uploads/2014/05/as-a-man-thinketh-cover.png' 
+  }, 
+  { 
+	name: 'Fifty Shades of Grey', 
+	price: 6.5, 
+	pubdate: new Date('2011', '08', '01'), 
+	cover: 'https://upload.wikimedia.org/wikipedia/en/5/5e/50ShadesofGreyCoverArt.jpg' 
+  }  
 ];
 }]);
