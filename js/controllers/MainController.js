cff597d4fcd493d589d267137218a04134e2838b
Shinto Theruvil Manuel <shinto.manuel@gmail.com>
2016-08-27 23:39:34 +0530

    Display multiple products

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,9 +1,18 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Hello AngularJS!'; 
   $scope.promo = "Let's explore";
-  $scope.product = {
-	  name: 'The Book of Trees',
-	  price: 19,
-	  pubdate: new Date('2014', '03', '08')
-  };
+  $scope.products = [ 
+  { 
+	name: 'The Book of Trees', 
+	price: 19, 
+	pubdate: new Date('2014', '03', '08'), 
+	cover: 'img/the-book-of-trees.jpg' 
+  }, 
+  { 
+	name: 'Program or be Programmed', 
+	price: 8, 
+	pubdate: new Date('2013', '08', '01'), 
+	cover: 'img/program-or-be-programmed.jpg' 
+  } 
+];
 }]);
